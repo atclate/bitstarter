f387f5414a696489739e5b6a0f7257726ccbe7b0
Ubuntu <ubuntu@ip-10-166-101-144.us-west-1.compute.internal>
2013-07-12 03:25:17 +0000

    Reading text from index.html

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-var indexTxt = fs.readFileSync('index.html').toString();
+var indexBuf = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(indexTxt);
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
